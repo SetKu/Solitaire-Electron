86c6318ebf123f4b28a5a1d971274af01927fe06
SetKu <zacharymorden1@gmail.com>
2021-12-06 18:41:41 -0700

    Added the module uuid for state management and installed Webpack. Reorganized project.

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import Deck from './deck.js';
+function styleAllPiles() {
+    const piles = document.getElementsByClassName("pile");
+    const offsetStart = 5.5;
+    for (let i = 0; i < piles.length; i++) {
+        let element = piles.item(i);
+        for (let i = 0; i < element.children.length; i++) {
+            let child = element.children[i];
+            if (i == 0) {
+                continue;
+            }
+            child.setAttribute("style", `transform: translateY(-${offsetStart * i}rem);`);
+        }
+    }
+}
+styleAllPiles();
+let deck = Deck.newDeck();
+deck.shuffle();
+console.log(deck);
